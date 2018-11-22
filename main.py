f3a1dac0868ea613c2bbd523621632201b58a31b
klydd <39828344+klydd@users.noreply.github.com>
2018-11-23 00:00:09 +0300

    updates

--- main.py
+++ main.py
@@ -6,18 +6,35 @@ letters = {'q':'й','w':'ц', 'e':'у', 'r':'к', 't':'е', 'y':'н', 'u':'г', 
            '}':'ъ', 'a':'ф', 's':'ы', 'd':'в', 'f':'а', 'g':'п', 'h':'р', 'j':'о', 'k':'л', 'l':'д', ';':'ж',
            "'":'э', 'z':'я', 'x':'ч', 'c':'с', 'v':'м', 'b':'и', 'n':'т', 'm':'ь', ',':'б', '.':'ю', '/':'.'}
 
-def letters_swap():
-    buf = tk.clipboard_get()
-    for i in range (len (buf)):
-        bufm = buf.lower ()
-        
+inverted_letters = {value: key for key, value in letters.items()}
+
+def get_key(d, value):
+    for k, v in d.items():
+        if v == value:
+            return str(k)
+
+def letters_swap(buf):
+    buflow = buf.lower ()
+    paste = str()
+
+    print (buf)
     print ('test')
-    tk.clipboard_append ('My_text')
 
+    for i in buflow:
+        if i in letters:
+            paste += letters.get(i)
+        elif i in inverted_letters:
+            paste += inverted_letters.get(i)
+        else:
+            paste += i
 
+    tk.clipboard_clear ()
+    tk.clipboard_append (paste)
+
+print (inverted_letters)
 tk = Tk ()
 tk.withdraw ()
-print (buf)
+##print (buf)
 left_key = win32api.GetKeyState (0x25)
 right_key = win32api.GetKeyState (0x27)
 #0 or 1 - key up
@@ -27,7 +44,6 @@ while True:
     sleep (1)
     left_key = win32api.GetKeyState (0x25)
     right_key = win32api.GetKeyState (0x27)
-    if left_key < 0 and right_key < 0:
-        letters_swap ()
-
-
+    if left_key < 0 and right_key < 0:  
+        buf = tk.clipboard_get()
+        letters_swap (buf)
